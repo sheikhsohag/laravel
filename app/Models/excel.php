a482f6f2c44e62e098498f1edc61b4ab501e7b36
sheikhsohag <sheikhsohag000@gmail.com>
2025-05-04 09:06:14 +0600

    project set up

---
+++ app/Models/excel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class excel extends Model
+{
+    //
+
+    protected $fillable = [
+        'file'
+    ];
+}
---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\ExcelController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::prefix('excel')->group(function(){
+    Route::post('/',[ExcelController::class,'store']);
+});
