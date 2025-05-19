43a57f667181445ab46156c3b9c5211539b26a2d
sheikhsohag <sheikhsohag000@gmail.com>
2025-05-19 09:37:07 +0600

    file,array of file,and delete get all file in storage

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -35,22 +35,67 @@ class ImageController extends Controller
 
     public function store(Request $request){
         $request->validate([
-            'file'=>'required|file|mimes:pdf,png,jpeg,jpg|max:2048'
+            'files' => 'required|array',
+            'files.*'=>'required|file|mimes:pdf,png,jpeg,jpg|max:2048'
         ]);
 
-        $file = $request->file('file');
-        $fileExtension = $file->getClientOriginalExtension();
+        $files = $request->file('files');
 
-        $filePath = now()->format('ymd-him') . '.' . $fileExtension;
-        $path = Storage::disk('public')->putFileAs('images/uploads', $file, $filePath);
-        $response = image::create([
-            'file'=>$path
-        ]);
+        $responses = [];
 
+        foreach($files as $file)
+        {
+            $path = $this->storeImage($file);
+            $responses[] = image::create([
+                'file'=>$path
+                ]);
+        }
+       
         return response()->json([
             'success'=>true,
             "message"=>"successfull",
-            "data"=>$response
+            "data"=>$responses
         ]);
     }
+
+
+    public function storeImage($file)
+    {
+         $fileExtension = $file->getClientOriginalExtension();
+
+        $filePath = now()->format('ymd-him') . '.' . $fileExtension;
+        $path = Storage::disk('public')->putFileAs('images/uploads', $file, $filePath);
+        return $path;
+    }
+
+    public function deleteImage($filename)
+    {
+        $path = 'uploads/' . $filename;
+
+        if (Storage::disk('public')->exists($path)) {
+            Storage::disk('public')->delete($path);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Image deleted successfully.'
+            ]);
+        }
+
+        return response()->json([
+            'success' => false,
+            'message' => 'Image not found.'
+        ], 404);
+    }
+
+    function getAllUploadedFiles($asUrl = false)
+    {
+        $files = Storage::disk('public')->files('uploads');
+
+        if ($asUrl) {
+            return array_map(fn($file) => Storage::url($file), $files);
+        }
+
+        return $files; // e.g., ['uploads/pic1.jpg', ...]
+    }
+
 }
