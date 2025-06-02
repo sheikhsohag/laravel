63ec7fdae9ce2315d177358a93b1a42d3d288eb8
sheikhsohag <sheikhsohag000@gmail.com>
2025-06-02 22:20:39 +0600

    my job added

--- app/Http/Controllers/JobBulkMailController.php
+++ app/Http/Controllers/JobBulkMailController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Jobs\JobBulkMailJob;
 use App\Models\EmailJob;
+use App\Models\EmailLog;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Bus;
+
 
 class JobBulkMailController extends Controller
 {
@@ -29,6 +32,43 @@ class JobBulkMailController extends Controller
             $job[] = new JobBulkMailJob(['email'=>$recipient],$emailJob);
         }
 
+        $batch = Bus::batch($job)
+        ->then(function() use ($emailJob){
+            $emailJob->update([
+                "status"=>"completed",
+                "completed_at"=>now()
+            ]);
+        })
+        ->catch(function(Throwable $e) use ($emailJob){
+             $emailJob->update([
+                "status"=>"failed",
+                 "completed_at"=>now()
+             ]);
+        })
+        ->finally(function() use ($emailJob) {
+            $sentCount = EmailLog::where('email_job_id', $emailJob->id)
+                    ->where('status', 'sent')
+                    ->count();
+                    
+                $failedCount = EmailLog::where('email_job_id', $emailJob->id)
+                    ->where('status', 'failed')
+                    ->count();
+                    
+                $emailJob->update([
+                    'sent_count' => $sentCount,
+                    'failed_count' => $failedCount,
+                ]);
+        })
+        ->dispatch();
+
+        
+    $emailJob->update(['batch_id' => $batch->id]);
+
+        return response()->json([
+            'message' => 'Bulk email job dispatched',
+            'job_id' => $emailJob->id,
+            'batch_id' => $batch->id,
+        ], 202);
 
     }
 }
