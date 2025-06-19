35ddabb28deaa8fbfb624710af64b5327ff8bae9
sheikhsohag <sheikhsohag000@gmail.com>
2025-06-19 10:22:41 +0600

    x-data done

--- resources/views/webView/alpine.blade.php
+++ resources/views/webView/alpine.blade.php
@@ -7,4 +7,29 @@
     </div>  
 </div>
 
+<div class="text-3xl" x-data="{foo:'bar'}">
+    <span x-text="foo"></span>
+    
+    <div x-data="{bar: 'baz'}">
+        <span x-text="bar"></span>
+        <div x-data="{foo: 'bob'}">
+            <span x-text="foo" x-text="bar" ></span>
+            <span x-text="bar"></span>
+        </div>
+    </div>
+
+</div>
+
+<div x-data="{
+   open: false,
+   toggle(){
+   console.log('sohag');
+    this.open = !this.open;
+   }
+}">
+
+<button class="bg-green-400 p-2 rounded hover:bg-green-500 cursor-pointer"  @click="toggle()">Toggle button</button>
+<div x-show="open">this is toggle content</div>
+</div>
+
 <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
