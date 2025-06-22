933f9b6ebe96aca1adb7160f06ab09d61b1966de
sheikhsohag <sheikhsohag000@gmail.com>
2025-06-22 09:22:15 +0600

    x-init, x-show, x-bind

--- resources/views/webView/alpine.blade.php
+++ resources/views/webView/alpine.blade.php
@@ -28,8 +28,42 @@
    }
 }">
 
+
+<div x-data>
+    <span x-init="console.log('I can initialize')"></span>
+</div>
+ 
+<span x-init="console.log('I can initialize too')"></span>
+
+<div x-data="{
+    init() {
+        console.log('I am called automatically')
+    }
+}">
+
+</div>
+
 <button class="bg-green-400 p-2 rounded hover:bg-green-500 cursor-pointer"  @click="toggle()">Toggle button</button>
 <div x-show="open">this is toggle content</div>
+</div>
+
+
+<div x-data="{
+    open: false,
+    toggle(){
+        console.log('clicked here', open);
+        this.open= !this.open;
+    }
+}">
+
+<button class="btn bg-amber-500 mt-10 p-2 rounded ml-3 hover:bg-amber-600" @click="toggle()">show content</button>
+
+
+<div x-show="open" x-cloak >this is hiden content! Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet voluptate consequuntur ducimus obcaecati. Aspernatur illo labore saepe. Dolorem, facere fugiat!</div>
+</div>
+
+<div x-data="{ placeholder: 'Type here...' }">
+    <input type="text" x-bind:placeholder="placeholder">
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
