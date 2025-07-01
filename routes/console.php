d13db0325d7eda3e6d5af3e21f8695393d4f558b
sheikhsohag <sheikhsohag000@gmail.com>
2025-07-01 08:26:15 +0600

    scheduler done

--- routes/console.php
+++ routes/console.php
@@ -10,6 +10,21 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 
-Schedule::call(function () {
-    Log::info('this is from scheduler');
-})->everyFiveSeconds();
+// Schedule::call(function () {
+//     Log::info('this is from scheduler');
+// })->everyFiveSeconds();
+
+// Schedule::call(function (){
+//     Log::info(
+//         "this is second!"
+//     );
+// })->everyTwoSeconds();
+
+// Artisan::command('log', function () {
+//     Log::info('this is second way of scheduler');
+// })->purpose('Log in the laravel.log file');
+
+// // Scheduling the command
+// Schedule::command('log')->everyTwoSeconds();
+
+Schedule::command('log-info-command')->everySecond();
