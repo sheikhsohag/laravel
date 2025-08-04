0fd061caf90727452cdab4ecebdb0a1e043ba2c8
sheikhsohag <sheikhsohag000@gmail.com>
2025-08-04 07:47:00 +0600

    laravel telescope apply

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,7 +3,7 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\CustomMacroProvider::class,
-    // App\Providers\MacroProvider::class,
     App\Providers\PaymentServiceProvider::class,
     App\Providers\SmsServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
 ];
